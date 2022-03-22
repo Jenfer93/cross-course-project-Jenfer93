bc267cae9702ff66940428ed48c5c9d15af51cac
Jenfer93 <89216683+Jenfer93@users.noreply.github.com>
2022-03-22 21:58:39 +0100

    updated form

--- js/details.js
+++ js/details.js
@@ -1,4 +1,4 @@
-import { products } from "./array.js";
+/*import { products } from "./array.js";
 
 const queryString = document.location.search; 
 
@@ -6,14 +6,50 @@ const params = new URLSearchParams(queryString);
 
 const  jacketId = params.get("id");
 
-function createDetail(){
-  for (let i = 0; products.length; i++){
 
-    if (products[i].id !== jacketId ) {
-      console.log([i]);
+products.forEach (function (product) {
+  if (products.id === jacketId) {
+    return true;
+  }
+})
+ console.log (products);
+
+
+//console.log(jacketId);
+
+/*function createDetail(){
+  for (let i = 0; i < products.length; i++){
+    if (jacketId !== products[i].id){
+      console.log(products[i].id);
+    } else {
+      console.log("obs something is wrong");
     }
-
   }
 }
 
-createDetail();
+createDetail();*/
+
+const modal = document.querySelector(".added-item");
+const addItem = document.querySelector(".cta-add-to-cart");
+const continueShopping = document.querySelector(".continue");
+const itemsInCart = document.querySelector(".items");
+
+
+
+addItem.addEventListener("click", ()=> {
+  modal.showModal();
+
+})
+
+continueShopping.addEventListener("click", ()=> {
+  modal.close();
+})
+
+//Add items to cart
+
+addItem.addEventListener("click", ()=> {
+  itemsInCart.innerHTML += `
+  <p>1 x Jacket</p>
+  <p>$ 99</p>`
+
+})
